b4670f5da70c0f7b6ac0b106be444bf786085523
MykHor <48554750+GurdosAres@users.noreply.github.com>
2024-01-11 23:02:46 +0100

    Add files via upload
    Comercial update

--- script.js
+++ script.js
@@ -3,3 +3,46 @@ function CloseOpen() {
     HamBtn.classList.toggle("IsActive");
     menuVisibility.classList.toggle("visibility");
 }
+let options = {
+    root: document.querySelector("#scrollArea"),
+    rootMargin: "0px",
+    threshold: 1.0,
+  };
+
+  const targets = document.querySelectorAll('.targetObs')
+
+  const lazyLoad = target => {
+    const io = new IntersectionObserver((entries,observer) => {
+
+        entries.forEach(entry => {
+            console.log("work!")
+            if(entry.isIntersecting){
+                const obj = entry.target
+              
+
+                if(entry.target.localName === "img"){
+                    const src = obj.getAttribute('data-lazy')
+                    obj.setAttribute('src',src)
+                    obj.classList.add('fade')
+                }else{
+                    obj.classList.add('fade')
+                }
+
+
+                observer.disconnect()
+            }
+        })
+
+
+    })
+    io.observe(target)
+  }
+ targets.forEach(lazyLoad)
+
+
+
+
+
+
+
+
